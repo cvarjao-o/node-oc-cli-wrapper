f09072d1524c60f2c89ca8d1db898087afa47feb
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-10-15 22:12:19 -0700

    daily commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 'use strict';
+const log4js = require('@log4js-node/log4js-api');
+const logger = log4js.getLogger('node-oc-cli-wrapper');
 const {spawn, spawnSync} = require('child_process');
 const path = require('path');
 const crypto = require('crypto')
@@ -23,21 +25,20 @@ function asArray(args){
 }
 
 //https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-function warn(args){
-  console.log('\x1b[31m', args ,'\x1b[0m');
-}
+//function warn(args){
+//  logger.warn('\x1b[31m', args ,'\x1b[0m');
+//}
 
 //https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 function die(args){
-  console.log('\x1b[31m', args ,'\x1b[0m');
-  console.log('\x1b[31m', 'Exited!' ,'\x1b[0m');
+  logger.error('\x1b[31m', args ,'\x1b[0m');
   process.exit(1)
 }
 
 function check_prerequisites(){
   var proc=spawnSync('oc', ['whoami'])
   var errors = ''
-  //console.log(`oc (exit code = ${proc.status})`)
+  //logger.trace(`oc (exit code = ${proc.status})`)
   if (proc.status == 0){
     //no-op
   }else if (proc.status == 1){
@@ -53,7 +54,7 @@ function check_prerequisites(){
   }
 
   var proc2=spawnSync('git', ['version'])
-  //console.log(`git (exit code = ${proc2.status})`)
+  //logger.trace(`git (exit code = ${proc2.status})`)
   if (proc2.status == 0){
     //no-op
   }else{
@@ -71,18 +72,21 @@ function _oc (client) {
   return function create (args = [], stdin = null) {
     return new Promise(function(resolve, reject) {
       const _args = asArray(client.settings.options).concat(asArray(args))
-      console.log(`oc ${_args.join(' ')}`)
-      const process = spawn('oc', _args);
+      logger.trace(`oc ${_args.join(' ')}`)
+      const _options = {cwd:client.settings.cwd};
+      logger.trace(`cwd: ${_options.cwd}`)
+
+      const process = spawn('oc', _args, _options);
       let stdout=''
       if (stdin!=null){
         stdin(process.stdin)
       }
       process.stdout.on('data', (data) => {
-        //console.log(`1>${data}`)
+        //logger.trace(`1>${data}`)
         stdout+=data
       });
       process.stderr.on('data', (data) => {
-        console.log(`2>${data}`)
+        logger.trace(`2>${data}`)
       })
       process.on('exit', (code) => {
         resolve({'code':code, 'stdout':stdout})
@@ -121,18 +125,29 @@ function ocApply (client) {
   return function create (args = []) {
     var stdin=null;
     var items=null;
+    var tmpfile=null;
     if (!(args instanceof Array)) {
-      //console.log('!args instanceof Array')
+      //logger.trace('!args instanceof Array')
       if (!(args['filename'] instanceof String)){
-        //console.log('!args["filename"] instanceof String')
+        //logger.trace('!args["filename"] instanceof String')
         items=args['filename']
-        args['filename']='-'
-        stdin=(stdin)=>{stdin.setEncoding('utf-8'); stdin.write(JSON.stringify(items)); stdin.end();}
+        var json=JSON.stringify(items);
+        var jsonHash=gitHashString(json);
+        tmpfile=`/tmp/${jsonHash}.json`;
+
+        fs.writeFileSync(tmpfile, json)
+        args['filename']=tmpfile
+        //stdin=(stdin)=>{stdin.setEncoding('utf-8'); stdin.write(json); stdin.end();}
       }
     }
 
     const _args=['apply', '--output=json'].concat(asArray(args))
-    return client._raw(_args, stdin).then((result)=>{
+    return client._raw(_args, stdin)
+    .then((result)=>{
+      fs.unlinkSync(tmpfile)
+      return result
+    })
+    .then((result)=>{
       //json output is in stream format
       var items=result.stdout.split(/\n}\n{\n/);
       
@@ -144,7 +159,7 @@ function ocApply (client) {
         }else{
           value = '{' + value + '}'
         }
-        //console.log(`[${index}]=${value}`)
+        //logger.trace(`[${index}]=${value}`)
         items[index]=JSON.parse(value)
       });
       //require('fs').writeFile('_oc_apply_stdout.json', result.stdout, 'utf8')
@@ -165,11 +180,18 @@ const CONSTANTS = Object.freeze({
   }
 });
 
-function gitHashObject(resource){
+function gitHashString(itemAsString){
   var shasum = crypto.createHash('sha1');
-  var itemAsString = JSON.stringify(resource)
+  //var itemAsString = JSON.stringify(resource)
   shasum.update(`blob ${itemAsString.length + 1}\0${itemAsString}\n`);
   return shasum.digest('hex');
+}
+
+function gitHashObject(resource){
+  //var shasum = crypto.createHash('sha1');
+  var itemAsString = JSON.stringify(resource)
+  //shasum.update(`blob ${itemAsString.length + 1}\0${itemAsString}\n`);
+  return gitHashString(itemAsString)
 }
 
 const resource_transformers = {
@@ -179,7 +201,7 @@ const resource_transformers = {
     resource.metadata.annotations = resource.metadata.annotations || {}
   },
   ENSURE_METADATA_NAMESPACE: (resource, container, client)=>{
-    resource.metadata.namespace = resource.metadata.namespace || container.namespace
+    resource.metadata.namespace = resource.metadata.namespace || container.namespace || client.settings.options.namespace
   },
   ADD_CHECKSUM_LABEL: (resource)=>{
     resource.metadata.labels[CONSTANTS.ANNOTATIONS.TEMPLATE_HASH] = gitHashObject(resource)
@@ -187,12 +209,13 @@ const resource_transformers = {
   REMOVE_BUILD_CONFIG_TRIGGERS: (resource)=>{
     if (resource.kind === CONSTANTS.KINDS.BUILD_CONFIG) {
       if (resource.spec.triggers && resource.spec.triggers.length > 0){
-        warn(`WARN: '${resource.kind}/${resource.metadata.name}' .spec.triggers are being removed and will be managed by this build script`)
+        logger.warn(`'${resource.kind}/${resource.metadata.name}' .spec.triggers are being removed and will be managed by this build script`)
       }
       resource.spec.triggers = []
     }
   },
-  ADD_SOURCE_HASH: (resource)=>{
+  ADD_SOURCE_HASH: (resource, client)=>{
+    //logger.trace(`cwd:${client.settings.cwd}`)
     if (resource.kind === CONSTANTS.KINDS.BUILD_CONFIG) {
       //ugly way of guarantee safe navigation (nullable object within a path. e.g.: `resource.spec.source`)
       var contextDir=(((resource || {}).spec || {}).source || {}).contextDir || ''
@@ -200,11 +223,11 @@ const resource_transformers = {
 
       if (resource.spec.source.type === 'Git'){
         //git tree-hash are more stable than commit-hash
-        sourceHash = spawnSync('git', ['rev-parse', `HEAD:${contextDir}`]).stdout.toString().trim()
+        sourceHash = spawnSync('git', ['rev-parse', `HEAD:${contextDir}`], {'cwd':client.settings.cwd}).stdout.toString().trim()
       }else if (resource.spec.source.type === 'Binary'){
-        var rootWorkDir = spawnSync('git', ['rev-parse', '--show-toplevel']).stdout.toString().trim()
+        var rootWorkDir = spawnSync('git', ['rev-parse', '--show-toplevel'], {'cwd':client.settings.cwd}).stdout.toString().trim()
         var absoluteContextDir=path.join(rootWorkDir, contextDir)
-        console.log(`contextDir:${contextDir} \t absoluteContextDir:${absoluteContextDir}`)
+        logger.trace(`contextDir:${contextDir} \t absoluteContextDir:${absoluteContextDir}`)
         var hashes=[]
 
         //find . -type f -exec git hash-object -t blob --no-filters '{}' \;
@@ -218,7 +241,7 @@ const resource_transformers = {
               if (fs.statSync(abspath).isDirectory()) {
                 walk(abspath, basedir)
               }else{
-                var hash = spawnSync('git', ['hash-object', '-t', 'blob', '--no-filters', abspath]).stdout.toString().trim()
+                var hash = spawnSync('git', ['hash-object', '-t', 'blob', '--no-filters', abspath], {'cwd':client.settings.cwd}).stdout.toString().trim()
                 //console.dir({'name':name, 'hash':hash})
                 hashes.push({'name':abspath.substr(basedir.length + 1), 'hash':hash})
               }
@@ -240,7 +263,7 @@ const resource_transformers = {
         sourceHash = gitHashObject(hashes)
       }
 
-      //console.log(`sourceHash:${sourceHash} (${contextDir})`)
+      //logger.trace(`sourceHash:${sourceHash} (${contextDir})`)
       resource.metadata.labels[CONSTANTS.ANNOTATIONS.SOURCE_HASH] = sourceHash;
     }
   }
@@ -249,38 +272,205 @@ const resource_transformers = {
 function shortName (resource) {
   return resource.metadata.name
 }
+
 function fullName (resource) {
   return resource.kind + '/' + resource.metadata.name
 }
 
+function startBuild (client) {
+  return (resource) => {
+    return new Promise((resolve, reject) => {
+      resolve({'kind':'Build', 'metadata':{'name':`${resource.metadata.name}-00`}})
+    })
+  }
+}
+
+function _startBuild (client, buildConfig) {
+  return (builds) => {
+    return new Promise(function(resolve, reject){
+      client.startBuild(buildConfig).then(build =>{
+        builds.push(build)
+        resolve(builds)
+      })
+    });
+  }
+}
+
+function _buildNext (client, currentBuildConfig, buidlConfigs, indexOfPromissesByBuildConfig, indexOfBuildConfigDependencies, indexOfBuildConfigByOutputImageStream) {
+
+
+  return new Promise(function(resolve, reject){
+    var buildConfigFullName = fullName(currentBuildConfig);
+
+  })
+
+  const maxLoopCount = buildConfigs.length * 2
+  var currentBuildConfig = null
+  var currentLoopCount = 0
+
+  while((currentBuildConfig=buildConfigs.shift()) !== undefined){
+    var buildConfigFullName = fullName(currentBuildConfig)
+    var dependencies= indexOfBuildConfigDependencies.get(buildConfigFullName)
+    var resolved=true
+
+    logger.trace(`Trying to queue ${buildConfigFullName}`)
+
+    for (var i = 0; i < dependencies.length; i++) {
+      var imageStreamName=dependencies[i]
+      var parentBuildConfig = indexOfBuildConfigByOutputImageStream.get(imageStreamName)
+      if (parentBuildConfig !== undefined){
+        if (!indexOfPromissesByBuildConfig.has(parentBuildConfig)){
+          logger.trace(`Waiting for ${fullName(parentBuildConfig)}`)
+          resolved = false
+          break;
+        }
+      }
+    }
+    //analize
+
+    //return to list, try again later
+    if (!resolved){
+      buildConfigs.push(currentBuildConfig)
+    }else{
+      var _callStartBuild = function(builds) {
+        var promise = new Promise(function(resolve, reject){
+          logger.trace(`Starting ${buildConfigFullName}`)
+           resolve(builds)
+        });
+
+        return promise;
+     };
+
+     logger.trace(`Queuing ${buildConfigFullName}`)
+     p=p.then(_startBuild(client, currentBuildConfig))
+
+      indexOfPromissesByBuildConfig.set(currentBuildConfig, p)
+    }
+
+    currentLoopCount++
+    if (currentLoopCount > maxLoopCount){
+      throw `It seems like there is a circular reference among BuildConfigs and their ImageStreams: ${buildConfigs}`
+    }
+  } // end while
+}
 function startBuilds (client) {
   return (resources) => {
+    logger.trace('>startBuilds')
     var promises = [];
+    //var keys = {}
+    var indexOfAllByName = new Map()
     var indexOfBuildConfigByOutputImageStream = new Map()
     var indexOfBuildConfigByInputImageStream = new Map()
     var indexOfBuildConfigDependencies = new Map()
-    var deployments = resources.filter((item) => { item.kind === CONSTANTS.KINDS.BUILD_CONFIG })
+    var indexOfBuildConfigBuild = new Map()
+    var indexOfPromissesByBuildConfig = new Map()
 
-    deployments.forEach((dc)=>{
-      var buildStrategy = dc.spec.strategy.sourceStrategy;
-      if (dc.spec.strategy.dockerStrategy != null){
-        buildStrategy=dc.spec.strategy.dockerStrategy
-        var dependencies = []
-        if (buildStrategy.from){
-          if (buildStrategy.from.kind === CONSTANTS.KINDS.IMAGE_STREAM_TAG){
-            //HERE
-          }else{
-            die(`Expected '${CONSTANTS.KINDS.IMAGE_STREAM_TAG}' but found '${buildStrategy.from.kind}' in ${fullName(dc)}.strategy.*.from`)
-          }
+    var buildConfigs = []
+    resources.forEach((res)=>{
+      var resourceFullName=fullName(res)
+      logger.trace(`Indexing ${resourceFullName} - ${res.metadata.namespace}`)
+      //keys[resourceFullName] = Symbol(resourceFullName)
+      indexOfAllByName.set(resourceFullName, res)
+      if (res.kind == CONSTANTS.KINDS.BUILD_CONFIG){
+        buildConfigs.push(res)
+      }
+    })
+
+    buildConfigs.forEach((bc)=>{
+      var buildConfigFullName = fullName(bc)
+      logger.trace(`Analyzing ${buildConfigFullName} - ${bc.metadata.namespace}`)
+      var outputTo = bc.spec.output.to
+      if (outputTo){
+        if (outputTo.kind === CONSTANTS.KINDS.IMAGE_STREAM_TAG){
+          var name=outputTo.name.split(':')
+          var imageStreamFullName = `${CONSTANTS.KINDS.IMAGE_STREAM}/${name[0]}`
+          indexOfBuildConfigByOutputImageStream.set(imageStreamFullName, bc)
+        }else{
+          die(`Expected '${CONSTANTS.KINDS.IMAGE_STREAM_TAG}' but found '${outputTo.kind}' in ${buildConfigFullName}.spec.output.to`)
+        }
+        
+      }
+      var buildStrategy = bc.spec.strategy.sourceStrategy || bc.spec.strategy.dockerStrategy
+
+      var dependencies = []
+      if (buildStrategy.from){
+        logger.trace(`${buildConfigFullName} - `, buildStrategy.from)
+        if (buildStrategy.from.kind === CONSTANTS.KINDS.IMAGE_STREAM_TAG){
+          var name=buildStrategy.from.name.split(':')
+          var imageStreamFullName = `${CONSTANTS.KINDS.IMAGE_STREAM}/${name[0]}`
+          indexOfBuildConfigByInputImageStream.set(imageStreamFullName, bc)
+          dependencies.push(imageStreamFullName)
+        }else{
+          die(`Expected '${CONSTANTS.KINDS.IMAGE_STREAM_TAG}' but found '${buildStrategy.from.kind}' in ${buildConfigFullName}.strategy.*.from`)
         }
       }
 
+      if ((bc.spec.source || {}).images){
+        var sourceImages= bc.spec.source.images
+        sourceImages.forEach( sourceImage  => {
+          if (sourceImage.kind === CONSTANTS.KINDS.IMAGE_STREAM_TAG){
+            var name=sourceImage.name.split(':')
+            var imageStreamFullName = `${CONSTANTS.KINDS.IMAGE_STREAM}/${name[0]}`
+            dependencies.push(imageStreamFullName)
+          }
+        })
+      }
+      indexOfBuildConfigDependencies.set(buildConfigFullName, dependencies)
     })
+
+    const maxLoopCount = buildConfigs.length * 2
+    var p = Promise.resolve([])
+    var currentBuildConfig = null
+    var currentLoopCount = 0
+
+    while((currentBuildConfig=buildConfigs.shift()) !== undefined){
+      var buildConfigFullName = fullName(currentBuildConfig)
+      var dependencies= indexOfBuildConfigDependencies.get(buildConfigFullName)
+      var resolved=true
+
+      logger.trace(`Trying to queue ${buildConfigFullName}`)
+
+      for (var i = 0; i < dependencies.length; i++) {
+        var imageStreamName=dependencies[i]
+        var parentBuildConfig = indexOfBuildConfigByOutputImageStream.get(imageStreamName)
+        if (parentBuildConfig !== undefined){
+          if (!indexOfPromissesByBuildConfig.has(parentBuildConfig)){
+            logger.trace(`Waiting for ${fullName(parentBuildConfig)}`)
+            resolved = false
+            break;
+          }
+        }
+      }
+      //analize
+
+      //return to list, try again later
+      if (!resolved){
+        buildConfigs.push(currentBuildConfig)
+      }else{
+        var _callStartBuild = function(builds) {
+          var promise = new Promise(function(resolve, reject){
+            logger.trace(`Starting ${buildConfigFullName}`)
+             resolve(builds)
+          });
+
+          return promise;
+       };
+
+       logger.trace(`Queuing ${buildConfigFullName}`)
+       p=p.then(_startBuild(client, currentBuildConfig))
+
+        indexOfPromissesByBuildConfig.set(currentBuildConfig, p)
+      }
+
+      currentLoopCount++
+      if (currentLoopCount > maxLoopCount){
+        throw `It seems like there is a circular reference among BuildConfigs and their ImageStreams: ${buildConfigs}`
+      }
+    } // end while
 
     //Index BuildConfigs and ImageStreams
     //Ordering of BuildConfigs
-
-    return Promise.all(promises)
+    return p;
   };
 }
 
@@ -294,7 +484,7 @@ function prepare (client) {
         resource_transformers.ADD_CHECKSUM_LABEL(item)
         resource_transformers.ENSURE_METADATA_NAMESPACE(item, list, client)
         resource_transformers.REMOVE_BUILD_CONFIG_TRIGGERS(item),
-        resource_transformers.ADD_SOURCE_HASH(item)
+        resource_transformers.ADD_SOURCE_HASH(item, client)
       })
 
       return resolve(list);
@@ -314,7 +504,8 @@ function openShiftClient (settings = {}) {
   client['process'] = ocProcess(client)
   client['apply'] = ocApply(client)
   client['prepare'] = prepare(client)
-
+  client['startBuild'] = startBuild(client)
+  client['startBuilds'] = startBuilds(client)
   return client;
 }
 
--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,119 @@
-var assert = require('assert');
+const assert = require('assert');
+const fs = require('fs');
+const crypto = require('crypto');
+const expect = require('expect.js');
+
+var mockSpawn = require('mock-spawn');
+var mySpawn = mockSpawn();
+var mockSpawnSync = (command, args)=>{
+  console.log(`command:${command}`)
+  console.log(`args:${args}`)
+  return {status:0}
+}
+
+//require('child_process').spawnSync = mockSpawnSync;
+var originalSpawn =require('child_process').spawn;
+//require('child_process').spawn = mySpawn;
+
+function hashObject(resource){
+  var shasum = crypto.createHash('sha1');
+  var itemAsString = JSON.stringify(resource)
+  shasum.update(itemAsString);
+  return shasum.digest('hex');
+}
+
+mySpawn.setStrategy(function (command, args, opts) {
+  console.log(`command:${command}`)
+  console.log(`args:${args.join(' ')}`)
+  console.log(`opts:${opts}`)
+
+  return function (cb) {
+    if (args.indexOf("process")){
+
+    }
+    var hash = hashObject({'command':command, 'args':args}) 
+    var json = require(`./mocks/${hash}.json`); //(with path)
+    this.stdout.write(json);
+    return cb(0); // and exit 0
+  };
+});
+
+const cli=require('./main')
+
+const buildConfigs=[{
+  'filename':'.pipeline/_python36.bc.json',
+  'param':[
+    'NAME=hello',
+    'SUFFIX=-prod',
+    'VERSION=1.0.0',
+    'SOURCE_BASE_CONTEXT_DIR=app-base',
+    'SOURCE_CONTEXT_DIR=app',
+    'SOURCE_REPOSITORY_URL=https://github.com/cvarjao-o/hello-world.git',
+    'SOURCE_REPOSITORY_REF=master'
+  ]
+}]
+
+
+var log4js = require('log4js');
+log4js.configure({
+  appenders: {
+    console: { type: 'console' }
+  },
+  categories: {
+    default: { appenders: ['console'], level: 'trace' }
+  }
+});
+
+var logger = log4js.getLogger();
+
+function save(object){
+  var hash= hashObject(object)
+  logger.info(`Writing output to ./src/mocks/${hash}.json`)
+  if (!fs.existsSync('./state')){
+    fs.mkdirSync('./state')
+  }  
+  fs.writeFileSync(`./state/${hash}.json`, JSON.stringify(object))
+}
+
+function restore(hash){
+  var content = fs.readFileSync(`./state/${hash}.json`);
+  return JSON.parse(content);
+}
+
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+    const oc=cli({'options':{'namespace':'csnr-devops-lab-tools'}, 'cwd':'/Users/cvarjao/Documents/GitHub/cvarjao-o/hello-world'});
+
+    before(function() {
+
+    })
+    after(function() {
+
+    })
+    it.skip('process/prepare/apply', function() {
+      this.timeout(20000);
+      return oc.process(buildConfigs)
+      .then((result) =>{
+        return oc.prepare(result)}
+      )
+      .then((result)=>{
+        save(result)
+        return oc.apply({'filename':result, 'dry-run':'true'});
+      })
+      .then((result)=>{
+        expect(result.length).to.equal(5);
+      })
     });
+
+    it('startBuilds', function() {
+      return new Promise(function(resolve, reject) {
+        resolve(restore('da2af20802a197321ba64a71e97915327d7826b1'))
+      }).then(result => {
+        return oc.startBuilds(result.items)
+      }).then(result  => {
+        console.dir(result)
+        expect(result.length).to.equal(2);
+      })
+    })
   });
 });
